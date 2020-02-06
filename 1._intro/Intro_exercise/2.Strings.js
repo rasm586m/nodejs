c08ac955ec3e1a9a7bc202aa26463007bb5b2947
Rasmus W <rasm586m@stud.kea.dk>
2020-02-06 10:49:51 +0100

    Fun with Strings

---
+++ 1._intro/Intro_exercise/2.Strings.js
@@ -0,0 +1,36 @@
+var numberOne = "1.10";
+var numberTwo = "2.30";
+var sum = Number(numberOne) + Number(numberTwo);
+
+console.log(sum);
+
+//
+
+var result = (parseFloat(numberOne) + parseFloat(numberTwo)).toFixed(2);
+
+console.log(result);
+console.log(parseFloat(result));
+
+//
+
+var one = 10;
+var two = 45;
+var three = 98;
+var total = ((one + two + three) /3).toFixed(5);
+
+console.log(total);
+
+//
+
+var letters = "abc";
+var indexOfLetters = letters.indexOf("c");
+var anotherWay = letters.charAt(2);
+
+console.log(indexOfLetters);
+
+//
+
+var fact = "You are learning javascript!";
+var capJ = fact.replace('j' , 'J');
+
+console.log(capJ);
